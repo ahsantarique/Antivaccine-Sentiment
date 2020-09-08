ddb2cf2152a606dc7d5d95ef4953e321142f6adb
Ahsan Tarique <ahsantarique@d-172-26-114-76.dhcp.virginia.edu>
2020-09-08 07:56:38 -0400

    files renamed and modulation completer up to seqbr

--- Src/init_sentiment.py
+++ Src/init_sentiment.py
@@ -1,5 +1,7 @@
+import numpy as np
+
+
 def random_get_init_sentiments(graph, PROB_ANTI):
-  random.seed(42)
 
   PROB_PRO = 1-PROB_ANTI
   initial_sentiments = np.random.choice(2, len(graph), p=[PROB_PRO, PROB_ANTI])
